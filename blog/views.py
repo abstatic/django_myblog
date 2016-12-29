ddbe2a8d871521a118b1c5ae96cb1afac091b30e
Abhishek Shrivastava <abhishek.shrivastava.ts@gmail.com>
2016-12-30 01:51:49 +0530

    Added comment models. Login fields

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 # Create your views here.
 
@@ -67,3 +67,18 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
